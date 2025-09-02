f3a40c695d049f2ef98b1e246c8469a5ea535ae6
Thubelihle Dlamini <thubelihledlamini88@gmail.com>
2025-09-02 11:17:05 +0200

    Update

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -10,12 +10,12 @@ ORDER BY
     total_bookings DESC;
 
 -- Query 2: Rank properties based on the total number of bookings using a window function.
--- This query uses RANK() to handle ties in the number of bookings.
+-- This query uses ROW_NUMBER() to rank properties.
 SELECT
     p.id,
     p.name,
     COUNT(b.id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.id) DESC) AS ranking
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS ranking
 FROM
     properties AS p
 LEFT JOIN
