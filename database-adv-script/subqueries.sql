3c378e5cea5df759ef3b74c78cc874a556b9d846
Thubelihle Dlamini <thubelihledlamini88@gmail.com>
2025-09-02 11:13:38 +0200

    Create subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,34 @@
+-- Query 1: Find properties with an average rating greater than 4.0 using a non-correlated subquery.
+SELECT
+    id,
+    name
+FROM
+    properties
+WHERE
+    id IN (
+        SELECT
+            property_id
+        FROM
+            reviews
+        GROUP BY
+            property_id
+        HAVING
+            AVG(rating) > 4.0
+    );
+
+-- Query 2: Find users who have made more than 3 bookings using a correlated subquery.
+SELECT
+    id,
+    first_name,
+    last_name
+FROM
+    users AS u
+WHERE
+    (
+        SELECT
+            COUNT(*)
+        FROM
+            bookings AS b
+        WHERE
+            b.user_id = u.id
+    ) > 3;
